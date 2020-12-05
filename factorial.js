af0ab1a1fd18568837cc799aaf54df433a7c6d37
Matt Rosensweig <matthew.rosensweig@gmail.com>
2020-12-05 17:52:32 -0500

    Factorial solution

---
+++ factorial.js
@@ -0,0 +1,12 @@
+function factorial(n) {
+  if (n === 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
+console.log(factorial(4));
+console.log(factorial(3));
+console.log(factorial(6));
